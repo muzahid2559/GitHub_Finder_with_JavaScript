ced3773e0427d62ca6fdb8ab2522bfd276484590
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-14 18:01:05 +0600

    Show Alert and Clear Profile

--- js/script.js
+++ js/script.js
@@ -2,25 +2,24 @@ let searchBtn = document.querySelector('#searchBtn');
 let searchUser = document.querySelector('#searchUser');
 let ui = new UI();
 
-
-searchBtn.addEventListener('click',(e)=>{
-    let userText = searchUser.ariaValueMax;
-    if(userText != ''){
-
-        //Fetch API
+searchBtn.addEventListener('click', (e) => {
+    let userText = searchUser.value;
+    if (userText != '') {
+        // Fetch API
         fetch(`https://api.github.com/users/${userText}`)
-        .then(result => result.json)
-        .then(data =>{
-            if(data.message == 'Not Found'){
-                //Show Alert
-            } else{
-                //Show Profile
-                ui.showProfile(data);
-            }
-        })
-
-
-    } else{
-        //Clear Profile
+            .then(result => result.json())
+            .then(data => {
+                //console.log(data);
+                if (data.message == 'Not Found') {
+                    // Show Alert
+                    ui.showAlert("User not Found!", "alert alert-danger");
+                } else {
+                    //Show Profile
+                    ui.showProfile(data);
+                }
+            })
+    } else {
+        // Clear Profile
+        ui.clearProfile();
     }
 });
--- js/ui.js
+++ js/ui.js
@@ -4,15 +4,14 @@ class UI {
     }
 
     showProfile(user) {
+        this.clearAlert();
         this.profile.innerHTML = `
         <div class="card card-body mb-3">
         <div class="row">
-
           <div class="col-md-3">
             <img class="img-fluid mb-2" src="${user.avatar_url}">
             <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
           </div>
-
           <div class="col-md-9">
             <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
             <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
@@ -26,10 +25,30 @@ class UI {
               <li class="list-group-item">Member Since: ${user.created_at}</li>
             </ul>
           </div>
-
         </div>
       </div>
         `;
     }
-    
+
+    clearProfile() {
+        this.profile.innerHTML = "";
+    }
+
+    showAlert(message, className) {
+        this.clearAlert();
+        this.clearProfile();
+        let div = document.createElement('div');
+        div.className = className;
+        div.appendChild(document.createTextNode(message));
+        let container = document.querySelector('.searchContainer');
+        let search = document.querySelector('.search');
+        container.insertBefore(div, search);
+    }
+
+    clearAlert() {
+        let currentAlert = document.querySelector('.alert');
+        if(currentAlert) {
+            currentAlert.remove();
+        }
+    }
 }
